3cdf5d1d446903e5b0646e0bfcfa29aebd6f10ac
Gellert Berenyi <berenyi.gellert@gmail.com>
2017-02-24 13:10:38 -0500

    Add files via upload

---
+++ GBERENYI_Lab4_WWW.ps1
@@ -0,0 +1,17 @@
+﻿<#
+Filename: GBERENYI_Lab4_WWW.ps1"
+Purpose: Create and launch html file by redirecting text and invoking file
+Author: Gellert Berenyi
+Course: Win213; Gurwitz, M
+Lab 4
+#>
+
+$WebFilePath = "$HOME\Documents\Week 4\Lab 4\GBERENYI_Lab4_WebPage.html"
+"<!DOCTYPE HTML PUBLIC `"-//W3C//DTD HTML 4.01 Transitional//EN`"" > "$WebFilePath"
+"<HTML>" >> "$WebFilePath"
+"<Head><style>BODY{background-color:peachpuff;}</style></HEAD>" >> "$WebFilePath"
+"<BODY>" >> "$WebFilePath"
+"<H1>PowerShell Scripted Web Page</H1>" >> "$WebFilePath"
+"</BODY>" >> "$WebFilePath"
+"</HTML>" >> "$WebFilePath"
+Invoke-Item "$WebFilePath"
